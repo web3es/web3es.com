33b48ac03948376ccb5cf41b7d559c1a224fe474
mmednik <mmednik@gmail.com>
2022-03-25 13:52:01 -0300

    instalo google tag manager

--- pages/index.js
+++ pages/index.js
@@ -16,13 +16,20 @@ export default function Home() {
           content="web3, solidity, smart contract, smart contracts, ethereum, ethers, ethersjs, hardhat, etherscan, web3es"
         />
         <meta name="author" content="web3es" />
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PLSTMB6');`}}></script>
         <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Alata&display=swap"
           rel="stylesheet"
         />
       </Head>
-
+      <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PLSTMB6"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+      
       <main className={styles.main}>
         <h1 className={styles.title}>web3es</h1>
 
